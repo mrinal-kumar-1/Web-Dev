3411cd5aa332f8e037f169c4f0772d747a4687bf
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2023-07-18 13:11:37 +0530

    Initial commit

---
+++ HTML Projects/practice.js
@@ -0,0 +1,27 @@
+// let age = prompt("Enter your age", "");
+// if(age >= 18)
+// {
+//     console.log("You are allowed");
+// }
+// else{
+//     console.log("You are not allowed");
+// }
+
+// let names = ["Harish", "Harvinder", "Gujan"];
+
+// let i = 10;
+// do{
+//     console.log(`${i} is less than 6, because we are inside do while loop`);
+// }
+// while(i<6);
+// while(i<6);
+
+// let i = 10;
+// console.log( "Value of i is kk",i
+// );
+
+let greet = () => {
+    console.log("Hey! How are you?");
+}
+
+setTimeout(greet,5000);
